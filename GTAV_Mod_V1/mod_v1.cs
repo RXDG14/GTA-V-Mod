eca496e897558e6648db4fee3621e3a4ce64f6a4
Rakshit N Prabhu <prabhurakshit@gmail.com>
2024-05-01 22:29:59 +0200

    mod menu updated

--- GTAV_Mod_V1/mod_v1.cs
+++ GTAV_Mod_V1/mod_v1.cs
@@ -13,62 +13,30 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 public class mod_v1 : Script // Change "YouTubeTutorial" to the name of your program.
 {
-    Vector3 playerPos = new Vector3();
-    float playerHeading = 0;
-    bool canTeleport = false;
-    readonly ObjectPool pool = new ObjectPool();
-    NativeCheckboxItem checkbox2 = new NativeCheckboxItem("Checkbox", false);
-
+    private ObjectPool pool = new ObjectPool();
+    private readonly NativeMenu menu = new NativeMenu("LemonUI", "Welcome to LemonUI!");
 
     public mod_v1() // Change "YouTubeTutorial" to the name of your program.
     {
         Tick += OnTick;
         KeyUp += OnKeyUp;
         KeyDown += OnKeyDown;
-        //CreateMenu(); kind of like begin play/construction script in ue5
+
+        CreateMenu();
     }
 
     private void OnTick(object sender, EventArgs e)
     {
         pool.Process();
-        CheckIfMenuItemActivated();
+        
     }
+
 
     private void OnKeyDown(object sender, KeyEventArgs e)
     {
-        if (e.KeyCode == Keys.F8)// save location
-        {
-            playerPos = Game.Player.Character.Position;
-            canTeleport = true;
-            //Game.Player.Character.CanRagdoll = true;
-            //Vehicle playerVehicle = Game.Player.Character.CurrentVehicle; 
-            // Pass in the Hash name and the parameters listed on Native DB.
-            //Function.Call(Hash.SET_VEHICLE_RADIO_ENABLED, Game.Player.Character.CurrentVehicle, false);
-            Function.Call(Hash.ADD_EXPLOSION, Game.Player.Character.Position.X + 10f, Game.Player.Character.Position.Y, Game.Player.Character.Position.Z, 2, 3f, true, false, 1f, false);
-            //Function.Call(Hash.SET_CLOCK_TIME,12, 34, 56);
-            Notification.Show("Cash?");
-        }
-        if (e.KeyCode == Keys.F9 && canTeleport)// teleport
-        {
-            Game.Player.Character.Position = playerPos;
-            Game.Player.Character.Heading = playerHeading;// playerPos.ToHeading();
-        }
-        if (e.KeyCode == Keys.F10)
-        {
-            Vehicle spawnedCar = World.CreateVehicle(VehicleHash.Alpha, Game.Player.Character.Position + Game.Player.Character.ForwardVector * 2f, Game.Player.Character.Heading + 90);
-            Notification.Show("Car spawned");
-        }
-        if(e.KeyCode == Keys.F11)
-        {
-            Vehicle playerVehicle = Game.Player.Character.CurrentVehicle;
-            if(playerVehicle != null)
-            {
-                playerVehicle.Repair();
-            }
-        }
         if (e.KeyCode == Keys.F12)
         {
-            CreateMenu();
+            menu.Visible = !menu.Visible;
         }
     }
 
@@ -77,29 +45,19 @@ public class mod_v1 : Script // Change "YouTubeTutorial" to the name of your pro
         
     }
 
-    public void CreateMenu()
+    protected void CreateMenu()
     {
-        NativeMenu menu = new NativeMenu("Mod", "Name", "Description");
-        NativeItem item1 = new NativeItem("Explosion");
-        NativeItem item2 = new NativeItem("Teleport");
-        menu.Add(item1);
-        menu.Add(item2);
-        menu.Visible = true;
-        NativeCheckboxItem checkbox1 = new NativeCheckboxItem("Checkbox", false);
-        menu.Add(checkbox2);
-        pool.Add(menu);
-        //if (item1.sele)
-        //{
-          //  Notification.Show("lol");
-        //}
-    }
+        NativeItem regularItem = new NativeItem("Regular Item", "This is a regular NativeItem, you can only activate it.");
+        NativeCheckboxItem checkboxItem = new NativeCheckboxItem("Checkbox Item", false);
+        NativeDynamicItem<int> dynamicItem = new NativeDynamicItem<int>("Dynamic Item", 10);
 
-    public void CheckIfMenuItemActivated()
-    {
-        if(checkbox2.Checked)
-        {
-            Function.Call(Hash.ADD_EXPLOSION, Game.Player.Character.Position.X + 10f, Game.Player.Character.Position.Y, Game.Player.Character.Position.Z, 2, 3f, true, false, 1f, false);
-            checkbox2.Checked = false;
-        }
+        menu.Add(submenu);
+        menu.Add(regularItem);
+        menu.Add(checkboxItem);
+        menu.Add(dynamicItem);
+
+        pool.Add(menu);
+        pool.Add(submenu);
+        menu.Visible = true;
     }
 }
